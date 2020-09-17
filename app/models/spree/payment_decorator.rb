bd1904d4af09f6522fa7ae4642fa6ecf3b1944d6
Rodrigo Garcia <rodrigo@goop.com>
2020-09-17 13:25:19 -0700

    DIS-342 | Correct classes

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Payment.class_eval do
 
   #Scopes
-  scope :afterpay, -> { joins(:payment_method).where(spree_payment_methods: { type: Spree::Gateway::Afterpay.to_s }) }
+  scope :afterpay, -> { joins(:payment_method).where(spree_payment_methods: { type: Spree::Gateway::AfterpayPayments.to_s }) }
 
 end
